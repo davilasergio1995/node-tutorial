2b5cd6efb200fdc1c3b090bd04d254b3a0fc7d45
Sergio <snowhopfirado@gmail.com>
2021-10-24 00:59:31 -0400

    init

--- alessa.js
+++ alessa.js
@@ -1,20 +1,31 @@
 const http = require('http');
+const {readFile, writeFile} = require('fs');
 const path = require('path');
-const {readFile} = require('fs');
-
-const newHtml = path.join(__dirname, './content','./html-pages','index.html');
-
-const newServer = http.createServer((req, res) => {
-    res.writeHead(200,{'Context-type': 'text/html'});
-    readFile(newHtml, 'utf8', (err, data) => {
-        if (err) {
-            res.writeHead(404);
-            res.write('Error: file not found');
-        } else {
-        res.write(data);
-        res.end();
+const htmlPage = path.join(__dirname, './content','html-pages', 'index.html');
+const jsFile = path.join(__dirname, './content','html-pages', 'button.js');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type': 'text/html'})
+    let fileRead = (fileName) => {
+        return new Promise((resolve, reject) => {
+            readFile(fileName,null,(err, data) => {
+                if(err) {
+                    reject(fileName);
+                } else {
+                    res.write(data);
+                    res.end();
+                    resolve();
+                }
+            })
+        })
+    };
+    let printPage = async () => {
+        try {
+            await fileRead(htmlPage);
+        } catch (error) {
+            console.log(error);
         };
-    });
+    };
+    printPage();
 });
 
-newServer.listen(8000);
+server.listen(8000)
---
+++ content/html-pages/button.js
@@ -0,0 +1,6 @@
+let button = document.querySelector('#button');
+let ayyLmao = document.querySelector('#ayy-lmao');
+
+button.addEventListener('click', () => {
+    ayyLmao.style.color='blue';
+});
